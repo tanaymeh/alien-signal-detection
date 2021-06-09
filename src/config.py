c2a0373e7981f9bbd6d3ab41ef4d51ec2e07dc15
Tanay Mehta <heyytanay@gmail.com>
2021-06-09 09:42:34 +0530

    Added Models

---
+++ src/config.py
@@ -0,0 +1,9 @@
+class Config:
+    N_SPLITS = 3
+    model_name = 'vit_base_patch16_224'
+    resize = (224, 224)
+    TRAIN_BS = 32
+    VALID_BS = 16
+    num_workers = 8
+    NB_EPOCHS = 10
+    LABELS = 1
---
+++ src/models.py
@@ -0,0 +1,32 @@
+import torch
+import timm
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .config import Config
+
+class VITModel(nn.Module):
+    """
+    Model Class for VIT Model
+    """
+    def __init__(self, model_name=Config.model_name, pretrained=True):
+        super(VITModel, self).__init__()
+        self.backbone = timm.create_model(model_name, pretrained, in_chans=1)
+        self.backbone.head = nn.Linear(self.model.head.in_features, Config.LABELS)
+    
+    def forward(self, x):
+        x = self.backbone(x)
+        return x
+    
+class MLPMixer(nn.Module):
+    """
+    Model Class for MLP Mixer Model
+    """
+    def __init__(self, model_name=Config.model_name, pretrained=True):
+        super(MLPMixer, self).__init__()
+        self.backbone = timm.create_model(model_name, pretrained, in_chans=1)
+        self.backbone.head = nn.Linear(self.backbone.head.in_features, Config.LABELS)
+    
+    def forward(self, x):
+        x = self.backbone(x)
+        return x
